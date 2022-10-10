6bb6efb85f7e340e06d55047b63ca7c7664ccbfa
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-10-10 08:30:40 -0400

    Update Fig.4a_ANOVA_Lmer_Diversity_HNPF_ITS_Type×Season+(Site).r

--- 2_Statistics/Fig.4a_ANOVA_Lmer_Diversity_HNPF_ITS_Type×Season+(Site).r
+++ 2_Statistics/Fig.4a_ANOVA_Lmer_Diversity_HNPF_ITS_Type×Season+(Site).r
@@ -6,7 +6,6 @@ DESIGN <- read.csv("experimental_design.csv")
 setwd("~/R/Analysis/4_Paddy/HNPF/ITS")
 METADATA <- read.csv("diversity.csv",header=T,row.names=1) 
 
-design <- DESIGN
 target <- METADATA
 
 NAME1 <- "Type"
@@ -16,7 +15,7 @@ NAME3 <- "Site"
 # ANOVA
 Results <- NULL
 for (i in 1:ncol(target)){
-data <- cbind(design, as.numeric(target[,i]))
+data <- cbind(DESIGN, as.numeric(target[,i]))
 colnames (data)[ncol(data)] <- colnames(target)[i]
 data <- na.omit (data)
 
